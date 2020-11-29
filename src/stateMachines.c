1538d55c00401e5f2815cfdca6c46a57030cce46
ZanteZyaire <jazamora6@miners.utep.edu>
2020-11-29 01:24:56 -0700

    Reformatted state methods to reflect getButtonPress and lcd draw methods

--- src/stateMachines.c
+++ src/stateMachines.c
@@ -1,71 +1,99 @@
 #include <msp430.h>
-#include <libTimer.h>
-#include "stateMachines.h"
+#include "libTimer.h"
 #include "led.h"
 #include "buzzer.h"
+#include "lcdutils.h"
+#include "lcddraw.h"
+#include "stateMachines.h"
 
 int song[] = {660,660,660,510,660,770,380,510,380,320,440,480,450,430,380,660,760,860,700,760,660,520,580,480,510,380,320,440,480,450,430,380,660,760,860,700,760,660,520,580,480,500,760,720,680,620,650,380,430,500,430,500,570,500,760,720,680,620,650,1020,1020,1020,380,500,760,720,680,620,650,380,430,500,430,500,570,585,550,500,380,500,500,500,500,760,720,680,620,650,380,430,500,430,500,570,500,760,720,680,620,650,1020,1020,1020,380,500,760,720,680,620,650,380,430,500,430,500,570,585,550,500,380,500,500,500,500,500,500,500,580,660,500,430,380,500,500,500,500,580,660,870,760,500,500,500,500,580,660,500,430,380,660,660,660,510,660,770,380}; // frequencies for song
 
-static int ratioOne = 1100; 
-static int ratioTwo = 380;
-
-void buttonOneState() // Plays a siren noise
+void drawOctagon()
 {
-  static char goUp = 1; // Bool for increasing/decreasing frequency
-  
-  if (ratioOne > 1400) goUp = 0;
-  else if (ratioOne < 1100) goUp = 1;
-
-  if (goUp) ratioOne += 5;
-  else ratioOne -= 5;
-  
-  buzzer_set_period(2000000 / ratioOne);
-
-  static char redState = 0;
-
-  switch (redState) { // green off 80% of time
-  case 0: redState++;   red_on = 1; break;
-  case 1:
-  case 2:
-  case 3: redState++;   red_on = 0; break;
-  case 4: redState = 0; red_on = 0; break;
+  for (int i = 0; i < 10; i++) {
+    drawPixel(i, i, COLOR_WHITE);
   }
 }
 
-void buttonTwoState() // Plays a song
+void playSong()
 {
   static int i = 0;
-  static int timer = 0;
 
   if (i == 156) i = 0;
-  if (timer++ > 25) { // Plays each note for 100ms
-    timer = 0;
-    buzzer_set_period(2000000 / song[i++]);
-  }
   
-  static char redState = 0;
-
-  switch (redState) { // Green off 40% of the time
-  case 0:
-  case 1:
-  case 2: redState++; red_on = 1; break;
-  case 3: redState++; red_on = 0; break;
-  case 4: redState = 0; red_on = 0; break;
-  }
+  buzzer_set_period(2000000 / song[i++]);
 }
 
-void buttonThreeState() // Increases frequency by 5 every call
+void dimLCD()
 {
-  ratioTwo += 5;
-  if (ratioTwo > 6000) ratioTwo = 380;
+  static char dim[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+  static char i = 0;
+  static char state = 0;
+  static char count = 0;
+  static char goDown = 0;
   
-  buzzer_set_period(2000000 / ratioTwo);
-  red_on = 1;
+  if (dim[9] == 1) { goDown = 1; i = 9; }
+  else if (dim[0] == 0) { goDown = 0; i = 0; }
+
+  if (++count > 4)
+  {
+    if (goDown) dim[i--] = 0;
+    else dim[i++] = 1;
+    count = 0;
+  }
+  
+  switch (state++)
+  {
+  case 0: red_on = dim[0];            break;
+  case 1: red_on = dim[1];            break;
+  case 2: red_on = dim[2];            break;
+  case 3: red_on = dim[3];            break;
+  case 4: red_on = dim[4];            break;
+  case 5: red_on = dim[5];            break;
+  case 6: red_on = dim[6];            break;
+  case 7: red_on = dim[7];            break;
+  case 8: red_on = dim[8];            break;
+  case 9: red_on = dim[9]; state = 0; break;
+  }
+  
+  led_changed = 1;
+  led_update();
 }
 
-void buttonFourState() // Turns off green led and sounds
+void buttonOneState() 
 {
   buzzer_set_period(0);
-  red_on = 0;
+  clearScreen(COLOR_GREEN);
+  buzzer_set_period(1000);
+  clearScreen(COLOR_BLUE);
+  buzzer_set_period(1500);
+  clearScreen(COLOR_YELLOW);
+  buzzer_set_period(2000);
+  clearScreen(COLOR_PINK);
+  buzzer_set_period(0);
 }
 
+void buttonTwoState() 
+{
+  clearScreen(COLOR_RED);      
+  clearScreen(COLOR_BLUE);
+  clearScreen(COLOR_WHITE);
+  drawString8x12(20, 20, "MARIO", COLOR_BLACK, COLOR_WHITE);
+  drawString8x12(20, 40, "TIME", COLOR_BLACK, COLOR_WHITE);
+}
+
+void buttonThreeState()
+{
+  buzzer_set_period(0);
+  clearScreen(COLOR_ORANGE);      
+  clearScreen(COLOR_GREEN);
+  clearScreen(COLOR_GOLD);
+  clearScreen(COLOR_PURPLE);
+}
+
+void buttonFourState() 
+{
+  buzzer_set_period(0);
+  clearScreen(COLOR_BLACK);
+  drawOctagon();
+}

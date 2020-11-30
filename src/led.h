67fda3bd8f9b3551c2b8a5b1d45080818a53e8bf
ZanteZyaire <jazamora6@miners.utep.edu>
2020-11-29 22:23:49 -0700

    Cleanup

--- src/led.h
+++ src/led.h
@@ -1,6 +1,8 @@
 #ifndef led_included
 #define led_included
 
+#include <msp430.h>
+
 #define LED_RED BIT0               // P1.0
 #define LED_GREEN BIT6             // P1.6
 #define LEDS (BIT0 | BIT6)
@@ -8,7 +10,7 @@
 extern unsigned char red_on, green_on;
 extern unsigned char led_changed;
 
-void led_init();
-void led_update();
+void led_init();		/* initialize LEDs */
+void led_update();		/* update leds */
 
-#endif // included
+#endif

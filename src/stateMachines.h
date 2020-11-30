53975c377f5305ffebe51544e2028a0ed640fe23
ZanteZyaire <jazamora6@miners.utep.edu>
2020-11-29 17:12:45 -0700

    Made buttonOneState return a signed char

--- src/stateMachines.h
+++ src/stateMachines.h
@@ -1,11 +1,12 @@
 #ifndef stateMachine_included
 #define stateMachine_included
 
-void buttonOneState();
+signed char buttonOneState(int i);
 void buttonTwoState();
 void buttonThreeState();
 void buttonFourState();
 void playSong();
+void moveSquare();
 void dimLCD();
 
 #endif // included

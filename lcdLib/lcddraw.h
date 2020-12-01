de149df340f39fdf02830b3cd57f1de7c3f63279
ZanteZyaire <jazamora6@miners.utep.edu>
2020-11-30 17:48:27 -0700

    Added 8x12 font

--- lcdLib/lcddraw.h
+++ lcdLib/lcddraw.h
@@ -44,12 +44,17 @@ void clearScreen(u_int colorBGR);
 void drawString5x7(u_char col, u_char row, char *string, 
 		   u_int fgColorBGR, u_int bgColorBGR);
 
+void drawString8x12(u_char col, u_char row, char *string, 
+		   u_int fgColorBGR, u_int bgColorBGR);
+
 /** 5x7 font - this function draws background pixels
- *  Adapted from RobG's EduKit
+ *  Adapted from RobG's EduKi
  */
 void drawChar5x7(u_char col, u_char row, char c, 
 		 u_int fgColorBGR, u_int bgColorBGR);
 
+void drawChar8x12(u_char rcol, u_char rrow, char c,
+		  u_int fgColorBGR, u_int bgColorBGR);
 /** Draw rectangle outline
  *  
  *  \param colMin Column start
